ff40162b0aded9081beb72f8044d208bf11d72a8
maximakhmin <max.akhmin@yandex.ru>
2026-04-10 21:09:18 +0300

    Added laps parser

--- parser/database.py
+++ parser/database.py
@@ -37,6 +37,46 @@ class Session(Base):
     session_type = Column(Integer, ForeignKey('session_types.id'))
     datetime_start_utc = Column(DateTime)
 
+class Tyres(Base):
+    __tablename__ = 'tyres'
+    id = Column(Integer, primary_key=True) 
+    name = Column(String)
+
+class TrackStatuses(Base):
+    __tablename__ = 'track_statuses'
+    id = Column(Integer, primary_key=True) 
+    name = Column(String)
+ 
+
+class Laps(Base):
+    __tablename__ = 'laps'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    session_id = Column(Integer, ForeignKey('sessions.id'))
+
+    driver_abbr = Column(String)
+    driver_num = Column(Integer)
+
+    position = Column(Integer)
+
+    lap_time = Column(Float)
+    lap_s1_time = Column(Float)
+    lap_s2_time = Column(Float)
+    lap_s3_time = Column(Float)
+    lap_number = Column(Integer)
+    stint_number = Column(Integer)
+    tyre_age = Column(Integer)
+    tyre_type = Column(Integer, ForeignKey('tyres.id'))
+    is_deleted = Column(Boolean)
+
+    air_temp = Column(Float)
+    track_temp = Column(Float)
+    humidity = Column(Float)
+    is_rain = Column(Boolean)
+    wind_direction = Column(Integer)
+    wind_speed = Column(Float)
+
+    track_status = Column(Integer, ForeignKey('track_statuses.id'))
+
 
 engine = create_engine(f'postgresql://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@localhost:5432/f1pace')
 Base.metadata.create_all(engine)
